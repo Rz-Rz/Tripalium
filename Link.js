646586f811e45443b3b39f3cc0896aa8d8f1623b
Karim Dhrif <karimdhrif4444gmail.com>
2024-04-16 19:40:31 +0200

    fixed the Links not using the classes they were specified with

--- Link.js
+++ Link.js
@@ -3,14 +3,14 @@ import Didact from './Didact'
 
 import { routingContext } from './RoutingContext';
 
-export default function Link({ to, children }) {
+export default function Link({ to, className, children }) {
 	const navigate = (e) => {
 		e.preventDefault();
 		routingContext.navigate(to, {}, true);
 	};
 
 	return (
-		<a href={to} onClick={navigate}>
+		<a href={to} className={className} onClick={navigate}>
 		{children}
 		</a>
 	);
